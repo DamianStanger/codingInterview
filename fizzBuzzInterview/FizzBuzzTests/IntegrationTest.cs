167741f757f94dc22b389c6a2b5ef00aaac68911
DamianStanger <damian.stanger+github@gmail.com>
2014-04-03 14:11:17 +0100

    solution, project structure and integration test for fizzbuzz

---
+++ fizzBuzzInterview/FizzBuzzTests/IntegrationTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+using ConsoleApp;
+
+namespace FizzBuzzTests
+{
+    [TestFixture]
+    public class IntegrationTest
+    {
+        const string OneToThirty = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizz Buzz\n16\n17\nFizz\n19\nBuzz\nFizz\n22\n23\nFizz\nBuzz\n26\nFizz\n28\n29\nFizz Buzz";
+
+        [Test]
+        public void ShouldProduceFullFizzBuzzOutput()
+        {
+            var args = new[] {"30"};
+            var sb = new StringBuilder();
+            Console.SetOut(new StringWriter(sb));
+            
+            Program.Main(args);
+            string actual = sb.ToString();
+
+            actual.Should().Be(OneToThirty);
+        }
+
+    }
+}
---
+++ fizzBuzzInterview/readme.md
@@ -0,0 +1,35 @@
+# Rules of Fizz Buzz
+[https://en.wikipedia.org/wiki/Bizz_buzz](https://en.wikipedia.org/wiki/Bizz_buzz)
+## output should be 
+```
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+Fizz Buzz
+16
+17
+Fizz
+19
+Buzz
+Fizz
+22
+23
+Fizz
+Buzz
+26
+Fizz
+28
+29
+Fizz Buzz
+```
