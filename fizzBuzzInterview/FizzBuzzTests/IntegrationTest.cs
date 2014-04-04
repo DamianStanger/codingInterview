a08d528af5ed8481bad90d3ab8f50e7bfce27152
DamianStanger <damian.stanger+github@gmail.com>
2014-04-04 10:03:50 +0100

    changed readme instructions and the integration test for fizzbuzz

--- fizzBuzzInterview/FizzBuzzTests/IntegrationTest.cs
+++ fizzBuzzInterview/FizzBuzzTests/IntegrationTest.cs
@@ -10,7 +10,21 @@ namespace FizzBuzzTests
     [TestFixture]
     public class IntegrationTest
     {
-        const string OneToThirty = "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizz Buzz\n16\n17\nFizz\n19\nBuzz\nFizz\n22\n23\nFizz\nBuzz\n26\nFizz\n28\n29\nFizz Buzz";
+        private const string OneToThirty = "1\r\n2\r\nFizz\r\n4\r\nBuzz\r\nFizz\r\n7\r\n8\r\nFizz\r\nBuzz\r\n11\r\nFizz\r\n13\r\n14\r\nFizz Buzz\r\n16\r\n17\r\nFizz\r\n19\r\nBuzz\r\nFizz\r\n22\r\n23\r\nFizz\r\nBuzz\r\n26\r\nFizz\r\n28\r\n29\r\nFizz Buzz\r\n";
+        private const string OneToTwo = "1\r\n2\r\n";
+
+        [Test]
+        public void ShouldProduceShortFizzBuzzOutput()
+        {
+            var args = new[] { "2" };
+            var sb = new StringBuilder();
+            Console.SetOut(new StringWriter(sb));
+
+            Program.Main(args);
+            string actual = sb.ToString();
+
+            actual.Should().Be(OneToTwo);
+        }
 
         [Test]
         public void ShouldProduceFullFizzBuzzOutput()
@@ -18,12 +32,11 @@ namespace FizzBuzzTests
             var args = new[] {"30"};
             var sb = new StringBuilder();
             Console.SetOut(new StringWriter(sb));
-            
+
             Program.Main(args);
             string actual = sb.ToString();
 
             actual.Should().Be(OneToThirty);
-        }
-
+        }        
     }
 }
---
+++ shapesInterview/README.md
@@ -0,0 +1,17 @@
+# Shapes Interview (Basic C#) #
+This solution is designed to test very basic knowledge of C#, it does use unit tests using nunit but the candidate need not really know about unit testing or nunit to do this.
+
+## Tests ##
+Basic C# knowledge, consuming interfaces, constructors, simple logic. Creating simple classes and methods, foreach and or linq. Nuget and mocking frameworks.
+
+## Steps ##
+
+**First**: Get them to create a square class to fix the failing test.
+
+**Second**: Un-ignore the next test and create a service or something (its upto them) to fix this test also.
+
+**Third**: Ask the candidate to add a new method to the service to calculate the perimeter of a shape. (called totalPerimeter, takes an enumerable of IShape returns a double)
+
+- Add the property 'double Perimeter { get; }' to the IShape interface, and add also to the square class (but dont make it work, leave as unset empty property).
+- Include in the file 'ShapeServiceTests' into the project. (This might need modification depending on how the candidate did the second part but its quite generic)
+- Then ask them to use a mocking framework to test drive the creation of a totalPerimeter method so that no actual squares are used. (this should test basic knowledge of what mocking frameworks are and how to use nuget to get the package)
