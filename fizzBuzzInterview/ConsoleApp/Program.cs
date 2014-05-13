6b8dc63811d9139b1a8115e098e995890e7592db
DamianStanger <damian.stanger+github@gmail.com>
2014-05-13 14:14:02 +0100

    small change to class definition

--- fizzBuzzInterview/ConsoleApp/Program.cs
+++ fizzBuzzInterview/ConsoleApp/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-
-namespace ConsoleApp
+﻿namespace ConsoleApp
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {           
